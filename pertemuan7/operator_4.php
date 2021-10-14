a5d7574321f81e5e052d6940dd35b68ec97ac4fa
Atmayanti <mayablue74@gmail.com>
2021-10-14 17:13:31 +0700

    add file operator4.php

---
+++ pertemuan7/operator_4.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+    </head>
+    <body>
+        <?php
+            $x = 4;
+            $x += 3;
+            echo "hasil dari operasi += pada x tersebut adalah = $x";
+            $x = 4;
+            $x -= 3;
+            echo "<br> hasil dari operasi -= pada x tersebut adalah = $x";
+            $x = 4;
+            $x *= 3;
+            echo "<br> hasil dari operasi *= pada x tersebut adalah = $x";
+            $x = 4;
+            $x /= 3;
+            echo "<br> hasil dari operasi /= pada x tersebut adalah = $x";
+            $x = 4;
+            $x %= 3;
+            echo "<br> hasil dari operasi %= pada x tersebut adalah = $x";
+            $x = 4;
+            $x .= 3;
+            echo "<br> hasil dari operasi .= pada x tersebut adalah = $x";
+        ?>
+    </body>
+</html>

79c5ccce302a7940083bd5c7d812e289549b19a1
Atmayanti <mayablue74@gmail.com>
2021-11-05 07:23:23 +0700

    add file fungsi_1.php

---
+++ pertemuan11/fungsi_1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+    <head>
+    </head>
+    <body>
+        <h2>Fungsi</h2>
+        <?php 
+            function writeMsg(){
+                echo "Hello World!";
+            }
+            writeMsg();
+        ?>
+    </body>
+</html>

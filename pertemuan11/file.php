95179467ff819ad54f1f3ae08afec0c9ddd0f6ec
Atmayanti <mayablue74@gmail.com>
2021-11-12 10:07:18 +0700

    add file file.php

---
+++ pertemuan11/file.php
@@ -0,0 +1,12 @@
+<head>
+</head>
+<body>
+    <?php 
+        $myFile = "testFile.txt";
+        $fh = fopen($myFile, 'r');
+        $theData = fread($fh, 5);
+        fclose($fh);
+        echo $theData;
+    ?>
+</body>
+</html>
